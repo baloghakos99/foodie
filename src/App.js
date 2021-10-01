4ccc1af9dfd23ef8d4ff5d5f7dc8aae760c742ff
Balogh Akos <baloghakos@valami.com>
2021-10-01 14:45:41 +0200

    update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Detail from "./components/Detail";
 import Dishes from "./components/Dishes";
 import React, { useState } from "react";
 import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, ToggleButton } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export let NavContext = React.createContext("");
@@ -16,6 +16,28 @@ export default function App() {
     JSON.parse(localStorage.getItem("context"))
   );
   const [title, setTitle] = useState("");
+  const [toggle, setToggle] = useState(false);
+
+  const toggleButton = document.getElementsByName("toggle-button")[0];
+  const navBarLinks = document.getElementsByClassName("navbar-links")[0];
+
+  // const toggle = () => {
+  //   console.log("haloooo");
+  //   toggleButton.addEventListener("click", () => {
+  //     console.log("cdsff");
+  //     navBarLinks.classList.toggle("active");
+  //   });
+  // };
+
+  const valami = () => {
+    console.log("aaaaaa");
+    if (!toggle) {
+      setToggle(true);
+    } else {
+      setToggle(false);
+    }
+    console.log(toggle);
+  };
 
   const setNavContext = (selectedNav, title) => {
     setContext(selectedNav);
@@ -34,9 +56,9 @@ export default function App() {
       <div className="main-container">
         <NavContext.Provider value={navContext}>
           <TitleContext.Provider value={title}>
-            <Navbar bg="dark" variant="dark" className="navbar">
-              {/* <Navbar.Brand href="#home">Home</Navbar.Brand> */}
-              <Nav className="mr-auto">
+            {/* <Navbar bg="dark" variant="dark" className="navbar"> */}
+            {/* <Navbar.Brand href="#home">Home</Navbar.Brand> */}
+            {/* <Nav className="mr-auto">
                 <Link className="nav-link" to="/">
                   Home
                 </Link>
@@ -76,7 +98,67 @@ export default function App() {
                   Drinks
                 </Link>
               </Nav>
-            </Navbar>
+            </Navbar> */}
+            <nav className="navbar">
+              <div className="title">Foodie</div>
+              <a href="#" class="toggle-button" onClick={() => valami()}>
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+              </a>
+              <div className={toggle ? "navbar-links-active" : "navbar-links"}>
+                <ul>
+                  <li>
+                    <a
+                      onClick={() => setNavContext("Starter", "Starters")}
+                      className="nav-link"
+                      href="/starters"
+                    >
+                      Starters
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      onClick={() => setNavContext("Soup", "Soups")}
+                      className="nav-link"
+                      href="/soups"
+                    >
+                      Soups
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      onClick={() =>
+                        setNavContext("Main%20course", "Main courses")
+                      }
+                      className="nav-link"
+                      href="/main-courses"
+                    >
+                      Main courses
+                    </a>
+                  </li>
+
+                  <li>
+                    <a
+                      onClick={() => setNavContext("Desserts", "Desserts")}
+                      className="nav-link"
+                      href="/desserts"
+                    >
+                      Desserts
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      onClick={() => setNavContext("Drinks", "Drinks")}
+                      className="nav-link"
+                      href="/drinks"
+                    >
+                      Drinks
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
 
             <>
               <Switch>
